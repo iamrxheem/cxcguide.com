be464dd6e23bf255fafc8bd054349ea37ed826f5
iamrxheem <iamrxheem@protonmail.com>
2020-06-05 12:27:49 -0500

    Added in pages

--- src/pages/quiz/csec/chemistry.js
+++ src/pages/quiz/csec/chemistry.js
@@ -1,62 +1,11 @@
 import React from "react";
 
-import Layout from "../../../components/layout";
-import SEO from "../../../components/seo";
-import MCQ from "../../../common/MCQ";
-import MCQItem from "../../../common/MCQItem";
-
-// Common
-import Breadcrumb from "../../../common/breadcrumb";
-import BreadcrumbItem from "../../../common/breadcrumbItem";
-
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <Layout>
-          <SEO
-            keywords={[
-              `chemistry`,
-              `csec`,
-              `quiz`,
-              `online quiz`,
-              `cxc past papers`,
-              `cxc textbooks`,
-              `csec textbooks`,
-              `cxc study guide`,
-              `csec practice quiz`,
-              `cxc etest`,
-              `csec etest`,
-              `elearning`,
-              `online learning`,
-              `jamaica`
-            ]}
-            title="CSEC Chemistry Quiz"
-            description="Our online Chemistry test quiz will assist you to improve your knowledge in a fun interactive way, and prepare you for your CXC exams."
-          />
-
-          <section className="flex-1 full max-w-4xl px-4 mx-auto md:py-5">
-            <Breadcrumb>
-              <BreadcrumbItem title="Quiz" href="/quiz" notLast />
-              <BreadcrumbItem title="CSEC" href="/quiz/csec" notLast />
-              <BreadcrumbItem title="Chemistry" />
-            </Breadcrumb>
-          </section>
-
-          <section className="flex-1 full max-w-5xl sm:px-4 px-2 mx-auto">
-            <MCQ>
-              {/* Question */}
-              <MCQItem />
-            </MCQ>
-          </section>
-        </Layout>
-      </>
-    );
-  }
+function Page() {
+  return (
+    <>
+      <></>
+    </>
+  );
 }
 
 export default Page;
